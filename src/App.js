eb1199318e3f6145b8e995126a0577d726f754e0
burhanj888 <burhanj888@gmail.com>
2023-10-04 20:00:31 -0400

    Deploy

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './Component/ProfileSidebar/Sidebar';
 // import ComponentA from './Component/About/About';
 import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Portfolio from './Portfolio';
+import AboutMe from './Component/About/About';
+import 'font-awesome/css/font-awesome.min.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WorkExperience from './Component/WorkEx/Work';
+import ProjectsSection from './Component/Projects/Projects';
+import BlogSection from './Component/Blogs/Blogs';
+import ConnectWithMe from './Component/Connect/Connect';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Portfolio></Portfolio>}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <CenteredHello></CenteredHello>
+      <StickyNavbar></StickyNavbar>
+      <Container fluid>
+      <Row>
+        <Col md={3} className="sidebar pt-5">
+          <Profile />
+        </Col>
+        <Col md={9} className="scrollable-content pt-5">
+          <AboutMe />
+          
+          <WorkExperience></WorkExperience>
+          
+          <ProjectsSection></ProjectsSection>
+          
+          <BlogSection></BlogSection>
+          <ConnectWithMe></ConnectWithMe>
+        </Col>
+      </Row>
+    </Container>
+    </div>
     
   );
 }
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -35,25 +35,6 @@ function Portfolio() {
         </Col>
       </Row>
     </Container>
-      
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    {/* </div> */}
-    {/* <div> */}
-      
-      {/* Rest of your components */}
     </div>
   );
 }
